80f8597db6b48e6a5f1fbbf385ec81a11c41a1ff
Matthew Chan <matt16749@gmail.com>
2015-07-14 12:43:57 -0700

    changes way landingpage is fed

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,25 +1,32 @@
 class LandingController < ApplicationController
-   def index
-    render text: index_html
+  def index  
+    redis
+    index_key &&= "propelx-ember:#{index_key}"
+    index_key ||= redis.get("propelx-ember:current")
+    render text: redis.get(index_key)
   end
 
-  private
+  # def index
+  #   render text: index_html
+  # end
 
-  def index_html
-    redis.get "#{deploy_key}:index.html"
-  end
+  # private
 
-  # By default serve release, if canary is specified then the latest
-  # known release, otherwise the requested version.
-  def deploy_key
-    params[:version] ||= 'release'
-    case params[:version]
-    when 'release' then 'release'
-    when 'canary'  then  redis.lindex('releases', 0)
-    else
-      params[:version]
-    end
-  end
+  # def index_html
+  #   redis.get "#{deploy_key}:index.html"
+  # end
+
+  # # By default serve release, if canary is specified then the latest
+  # # known release, otherwise the requested version.
+  # def deploy_key
+  #   params[:version] ||= 'release'
+  #   case params[:version]
+  #   when 'release' then 'release'
+  #   when 'canary'  then  redis.lindex('releases', 0)
+  #   else
+  #     params[:version]
+  #   end
+  # end
 
   def redis
     if Rails.env.development?
